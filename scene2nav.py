8085ffbbf7a29711f6d8d2a558949e21f6229dbe
rbn42 <bl100@students.waikato.ac.nz>
2016-08-06 20:20:05 +1200

    format source

--- scene2nav.py
+++ scene2nav.py
@@ -9,7 +9,7 @@ Usage:
 
 Options:
   -h --help     Show this screen.
-""" 
+"""
 import numpy as np
 from config import random_seed
 
@@ -57,22 +57,24 @@ import logging
 '''
 我怀疑当游戏渲染窗口被遮蔽的时候会造成渲染不准确,最好尽量避免这样的情况
 '''
+
+
 class Demo(ShowBase):
 
    # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
 
-
-    def __init__(self, img_size=512, screen_off=True,target_area_radius=5,initial_area_radius=10, keyboard_input=False,random_reset_around_target =False,test=False):
-        logging.info('random_reset_around_target :%s',random_reset_around_target )
-        self.random_reset_around_target=random_reset_around_target 
+    def __init__(self, img_size=512, screen_off=True, target_area_radius=5, initial_area_radius=10, keyboard_input=False, random_reset_around_target=False, test=False):
+        logging.info('random_reset_around_target :%s',
+                     random_reset_around_target)
+        self.random_reset_around_target = random_reset_around_target
         self.keyboard_input = keyboard_input
         # Configure the parallax mapping settings (these are just the defaults)
         self.img_size = img_size
-        self.initial_area_radius=initial_area_radius
-        self.target_area_radius=target_area_radius
+        self.initial_area_radius = initial_area_radius
+        self.target_area_radius = target_area_radius
         loadPrcFileData("", "side-by-side-stereo 1")
         if test:
-            loadPrcFileData("", "load-display p3tinydisplay") 
+            loadPrcFileData("", "load-display p3tinydisplay")
         loadPrcFileData("", "transform-cache false")
         loadPrcFileData("", "audio-library-name null")  # Prevent ALSA errors
         loadPrcFileData("", "win-size %d %d" % (2 * img_size, img_size))
@@ -80,7 +82,8 @@ class Demo(ShowBase):
                             "parallax-mapping-scale 0.1")
 
         if screen_off:
-            loadPrcFileData("", "window-type offscreen")  # Spawn an offscreen buffer
+            # Spawn an offscreen buffer
+            loadPrcFileData("", "window-type offscreen")
         # Initialize the ShowBase class from which we inherit, which will
         # create a window and set up everything we need for rendering into it.
         ShowBase.__init__(self)
@@ -222,9 +225,9 @@ class Demo(ShowBase):
         self.ralphGroundHandler = CollisionHandlerQueue()
         self.cTrav.addCollider(cnodePath, self.ralphGroundHandler)
 
- 
-        cnodePath = self.navigation_target.attachNewNode(CollisionNode('target'))
-        cnodePath.node().addSolid(CollisionSphere(0, 0, 0, 2))  
+        cnodePath = self.navigation_target.attachNewNode(
+            CollisionNode('target'))
+        cnodePath.node().addSolid(CollisionSphere(0, 0, 0, 2))
         self.cTrav.addCollider(cnodePath, self.ralphGroundHandler)
 
         # Tell Panda that it should generate shaders performing per-pixel
@@ -248,35 +251,36 @@ class Demo(ShowBase):
         l = altCam.node().getLens()
         l.setFov(80)
         l.setNear(.1)
-        
-         
+
         camera.reparentTo(self.agent)
         camera.setZ(0.4)
         l = self.camLens
         # l.setFov(80)
         l.setNear(.1)
-        ################### end init
+        # end init
 
     def setKey(self, key, value):
         self.keyMap[key] = value
 
-    def step(self, action=(0, 0, 0)): 
+    def step(self, action=(0, 0, 0)):
         h, p, forward = action
         self.agent.setH(self.agent, h)
         self.agent.setP(self.agent, p)
         self.agent.setY(self.agent, forward)
-        relative_target_position = self.__get_relative_target_position()        
+        relative_target_position = self.__get_relative_target_position()
         if not None == self.relative_target_position:
-            speed = abs(relative_target_position) - abs(self.relative_target_position)
+            speed = abs(relative_target_position) - \
+                abs(self.relative_target_position)
             assert abs(speed) < 0.02 * 25 * 0.2 * 1.001 * 4
-        self.relative_target_position = relative_target_position    
-                
+        self.relative_target_position = relative_target_position
+
         return len(self.get_collision_list()) > 0
         dt = 0.02  # 控制跳帧,这个问题在转移到现实的时候需要考虑,摄影机如果能提供固定的拍摄频率就最好了,
         if len(self.pieces) > 0:
             for _ in range(5):
-                rng.choice(self.pieces).step(dt) 
-    def get_collision_list(self): 
+                rng.choice(self.pieces).step(dt)
+
+    def get_collision_list(self):
         if self.keyboard_input == False:
             f = taskMgr.getAllTasks()[-1].getFunction()
             f(None)
@@ -284,77 +288,85 @@ class Demo(ShowBase):
             f(None)
         else:
             taskMgr.step()
-        return (self.ralphGroundHandler.getEntries()) 
+        return (self.ralphGroundHandler.getEntries())
     relative_target_position = None
+
     def __get_relative_target_position(self):
         from_hpr = self.agent.getHpr()
         h = from_hpr[0] * math.pi / 180
-        
+
         from_pos = self.agent.getPos()
         from_pos = from_pos[0] + from_pos[1] * 1j
-        
-        target_pos = self.navigation_target.getPos() 
+
+        target_pos = self.navigation_target.getPos()
         target_pos = target_pos[0] + target_pos[1] * 1j
-        
+
         relative_pos = target_pos - from_pos
-        rpos = relative_pos * np.exp(-h * 1j) 
+        rpos = relative_pos * np.exp(-h * 1j)
         return rpos
-    
+
     def get_agent_position(self):
         return self.agent.getPos()
+
     def get_obstacle_positions(self):
         return [piece.knight.getPos() for piece in self.pieces]
+
     def get_obstacle_radius(self):
         return [piece.radius for piece in self.pieces]
+
     def get_target_position(self):
         return self.navigation_target.getPos()
-    
-    def resetGame(self): 
-        
+
+    def resetGame(self):
+
         self.renderFrame()
-        img=self.getScreen()
+        img = self.getScreen()
 
         for p in self.pieces:
             p.putaway()
-             
+
         self.agent.setPos((20, 20, 1000))
         size = 60
- 
-        while True: 
-            x,y =rng.uniform(-size,size,2)# rng.rand() * 
-            self.navigation_target.setPos((x, y, 0)) 
+
+        while True:
+            x, y = rng.uniform(-size, size, 2)  # rng.rand() *
+            self.navigation_target.setPos((x, y, 0))
             l = self.get_collision_list()
             if len(l) < 1:
-                break 
+                break
         # print(i)
         target_pos = self.navigation_target.getPos()
         target_pos = target_pos[0] + target_pos[1] * 1j
-        
+
         while True:
-            if not self.random_reset_around_target: #:
-                x,y =rng.uniform(-size,size,2)# rng.rand
+            if not self.random_reset_around_target:  # :
+                x, y = rng.uniform(-size, size, 2)  # rng.rand
             else:
                 while True:
-                    pos = (10 + size * math.pi * rng.rand()) * np.exp(rng.rand() * math.pi * 1j)
+                    pos = (10 + size * math.pi * rng.rand()) * \
+                        np.exp(rng.rand() * math.pi * 1j)
                     pos += target_pos
                     if -size < pos.real < size and -size < pos.imag < size:
                         x, y = pos.real, pos.imag
-                        break 
-            self.agent.setPos((x, y, 0)) 
+                        break
+            self.agent.setPos((x, y, 0))
             l = self.get_collision_list()
             if len(l) < 1:
-                break 
-            
-        self.agent.setH(rng.uniform(0,360))  
-        self.relative_target_position = self.__get_relative_target_position() 
+                break
+
+        self.agent.setH(rng.uniform(0, 360))
+        self.relative_target_position = self.__get_relative_target_position()
 
         for p in self.pieces:
-            p.reset(avoids=[(self.agent.getPos(),self.initial_area_radius),(self.navigation_target.getPos(),self.target_area_radius)])
+            p.reset(avoids=[(self.agent.getPos(), self.initial_area_radius),
+                            (self.navigation_target.getPos(), self.target_area_radius)])
         # print(i)
+
     def renderFrame(self):
         base.graphicsEngine.renderFrame()
+
     def getScreen(self):
-    #    base.graphicsEngine.renderFrame()
+        #    base.graphicsEngine.renderFrame()
         # self.screenshot(namePrefix=self._screenshot)
         tex = self.win.getScreenshot()
         r = tex.getRamImage()
@@ -451,16 +463,17 @@ class Piece:
 
         self.reset()
 
-
-    def reset(self,avoids=[]):
+    def reset(self, avoids=[]):
         self._speed = np.zeros(2)
         while True:
-            x,y =rng.uniform(-self.roomsize,self.roomsize,2)# rng.rand() * 2 * self.roomsize - self.roomsize        y = rng.rand() * 2 * self.roomsize - self.roomsize
-            piece_scale =rng.uniform(2,11)# 9 * rng.rand() + 2
-            for point,radius in avoids:
-                r=radius+0.27 * piece_scale*2
-                _x,_y,_z=point
-                if (_x-x)**2+(_y-y)**2<r**2:
+            # rng.rand() * 2 * self.roomsize - self.roomsize        y =
+            # rng.rand() * 2 * self.roomsize - self.roomsize
+            x, y = rng.uniform(-self.roomsize, self.roomsize, 2)
+            piece_scale = rng.uniform(2, 11)  # 9 * rng.rand() + 2
+            for point, radius in avoids:
+                r = radius + 0.27 * piece_scale * 2
+                _x, _y, _z = point
+                if (_x - x)**2 + (_y - y)**2 < r**2:
                     break
             else:
                 break
@@ -471,16 +484,14 @@ class Piece:
         # self.knight.reparentTo(render)
         # self.knight.setColor(color)
         self.knight.setScale(piece_scale)
-        self.radius=0.27*piece_scale
+        self.radius = 0.27 * piece_scale
+
     def putaway(self):
-        self.knight.setPos((1000,1000,1000))
-
-
-
-
+        self.knight.setPos((1000, 1000, 1000))
 
     def step(self, dt):
-        dx,dy =rng.uniform(- self.acceleration_max,self.acceleration_max,2)# * (2 * rng.rand() - 1)        dy = self.acceleration_max * (2 * rng.rand() - 1)
+        # * (2 * rng.rand() - 1)        dy = self.acceleration_max * (2 * rng.rand() - 1)
+        dx, dy = rng.uniform(- self.acceleration_max, self.acceleration_max, 2)
         self._speed += dx * dt, dy * dt
         norm = np.linalg.norm(self._speed)
         if norm > self.speed_max:
@@ -501,17 +512,15 @@ class Piece:
         self.model.setPos((self._pos[0], self._pos[1], 0))
 
 
-
- 
-if '__main__' == __name__: 
+if '__main__' == __name__:
     from docopt import docopt
     arguments = docopt(__doc__)
-    
-    if arguments['random']: 
+
+    if arguments['random']:
         demo = Demo(screen_off=False, keyboard_input=False)
     elif arguments['control']:
         demo = Demo(screen_off=False, keyboard_input=True)
-    
+
     self = demo
 #    mywin3 = self.openWindow()
 # #    from direct.gui.OnscreenImage import OnscreenImage
@@ -520,13 +529,14 @@ if '__main__' == __name__:
 # #    win2Cam2d = base.makeCamera2d(mywin3)
 # #    win2Cam2d.reparentTo(win2Render2d)
 # #    imageObject.reparentTo(win2Render2d)
-    p_depthmap='/dev/shm/s_depth.npy'
-    p_screen='/dev/shm/s_screen.npy'
-    p_stereo='/dev/shm/s_stereo.npy'
+    p_depthmap = '/dev/shm/s_depth.npy'
+    p_screen = '/dev/shm/s_screen.npy'
+    p_stereo = '/dev/shm/s_stereo.npy'
     while True:
         print(demo.agent.getPos())
-   #     print(demo.relative_target_position) 
-        action_options = [ (0, 0, 0.1),(2, 0, 0.1), (-2, 0, 0.1),(0,0,0) ]  # (0,0,0)]
+   #     print(demo.relative_target_position)
+        action_options = [(0, 0, 0.1), (2, 0, 0.1),
+                          (-2, 0, 0.1), (0, 0, 0)]  # (0,0,0)]
         if self.keyMap["left"]:
             action = action_options[1]
         elif self.keyMap["right"]:
@@ -535,26 +545,26 @@ if '__main__' == __name__:
             action = action_options[0]
         else:
             action = action_options[3]
-        if arguments['random']: 
-            action = action_options[rng.randint(len(action_options))] 
+        if arguments['random']:
+            action = action_options[rng.randint(len(action_options))]
             # c,p=demo.step(action)
         if demo.step(action):
-            demo.resetGame()  
-        if arguments['random']:  
+            demo.resetGame()
+        if arguments['random']:
             base.graphicsEngine.renderFrame()
         elif arguments['control']:
             taskMgr.step()
         if not os.path.exists(p_depthmap):
-            img=self.getDepthMapT()
-            np.save(p_depthmap,img)
+            img = self.getDepthMapT()
+            np.save(p_depthmap, img)
         if not os.path.exists(p_screen):
-            img=self.getScreen()
-            np.save(p_screen,img)
+            img = self.getScreen()
+            np.save(p_screen, img)
         if not os.path.exists(p_stereo):
-            img=self.getScreen()
-            img=self.getRawImageStereo(img)
-            np.save(p_stereo,img)
-        
+            img = self.getScreen()
+            img = self.getRawImageStereo(img)
+            np.save(p_stereo, img)
+
 #        if random.random() < 0.03:
 #            i = self.getDepthMapT()
 #            p = '/dev/shm/111.jpg'
